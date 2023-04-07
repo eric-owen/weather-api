1d5b42ee2729e29f0d84f1381836fa4fbb190758
eric-owen <ejo.owen@icloud.com>
2023-04-07 13:14:32 -0400

    adds api route for grabbing all data related to a single station

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 stations = pd.read_csv("data/stations.txt", skiprows=17)
 
+
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
@@ -13,7 +14,6 @@ def home():
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
-    print(filename)
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze()
     return {
@@ -21,6 +21,13 @@ def about(station, date):
         "date": date,
         "temperature": float(temperature)
     }
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    return df.to_dict(orient="records")
 
 
 if __name__ == "__main__":
